c4814071f80987f83ddd435b4e40e812ad0fc93c
MBL464 <zefanya.zulian18@mhs.ubharajaya.ac.id>
2026-05-04 15:42:29 +0700

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -38,7 +38,7 @@ export async function handler(event) {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
